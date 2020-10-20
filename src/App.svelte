9f7ce6ba961acff7e7cab1ffadcaa6a001d9acfc
Andrew Petersen <andrew.petersen15@gmail.com>
2020-10-19 20:18:52 -0500

    got the save the date looking okay

--- src/App.svelte
+++ src/App.svelte
@@ -4,22 +4,8 @@
   import SaveTheDate from "./SaveTheDate.svelte";
 </script>
 
-<style>
-  main {
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
 <Header />
 <main>
   <SaveTheDate />
-  <ImportGuests />
+  <!-- <ImportGuests /> -->
 </main>
--- src/Header.svelte
+++ src/Header.svelte
@@ -4,30 +4,47 @@
     justify-content: space-around;
     align-items: center;
     font-family: var(--fancyFont);
-  }
-  header .title {
-    font-size: 1.5rem;
-    color: rosybrown;
-  }
-  .acronym {
-    font-family: var(--bodyFont);
-    font-weight: 600;
-    font-size: 2em;
-    color: #000;
+    padding: 1rem 2rem;
   }
   header nav {
     display: flex;
+    width: 100%;
+    justify-content: flex-end;
+    font-size: 1.8rem;
   }
+
   header nav > * {
-    margin-left: 10px;
+    margin-left: 2rem;
+  }
+  .logo a {
+    opacity: .8;
+  }
+  .logo a:hover {
+    opacity: 1;
+  }
+  .logo img {
+    height: 30px;
+  }
+  @media (max-width:640px) {
+    header nav {
+      font-size: 1.4rem;
+      justify-content: center;
+    }
+    .logo {
+      display: none;
+    }
   }
 </style>
 
 <header>
-  <h2 class="title">Nat&DrewSay<span class="acronym">ID</span>o</h2>
+  <div class='logo'>
+    <a href="/" title='Nat & Drew Say I Do'>
+      <img src='/images/mountain-logo.svg' alt='mountain-logo'>
+    </a>
+  </div>
   <nav>
-    <a href="#">About us</a>
-    <a href="#">Getting There</a>
+    <a href="#">Travel</a>
+    <a href="#">Things to do</a>
     <a href="#">Registry</a>
   </nav>
 </header>
