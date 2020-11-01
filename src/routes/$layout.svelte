897139b4de69b450cfd70d1af1edc9d8fabde70e
Andrew Petersen <andrew.petersen15@gmail.com>
2020-11-01 14:02:44 -0700

    added new favicon and preloaded fonts

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -1,17 +1,20 @@
 <script>
-if (typeof window !== 'undefined') {
-    (function(history){
-        var pushState = history.pushState;
-        history.pushState = function(...args) {
-          console.log("history.pushState -> args", args)
-          setTimeout(() => window.dispatchEvent(new Event('locationchange')), 0);
-          return pushState.apply(history, args);
-        };
-        window.addEventListener("popstate", () => {
-          setTimeout(() => window.dispatchEvent(new Event('locationchange')), 0);
-        })
+  if (typeof window !== "undefined") {
+    (function (history) {
+      var pushState = history.pushState;
+      history.pushState = function (...args) {
+        console.log("history.pushState -> args", args);
+        setTimeout(() => window.dispatchEvent(new Event("locationchange")), 0);
+        return pushState.apply(history, args);
+      };
+      window.addEventListener("popstate", () => {
+        setTimeout(() => window.dispatchEvent(new Event("locationchange")), 0);
+      });
     })(window.history);
-}
+  }
 </script>
 
-<slot></slot>
+<svelte:head>
+  <title>06/19/2021</title>
+</svelte:head>
+<slot />
