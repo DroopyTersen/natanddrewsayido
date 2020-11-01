d2b72372a76c2c0f4f74c03c887cf368c82e5752
Andrew Petersen <andrew.petersen15@gmail.com>
2020-10-31 21:08:19 -0500

    tweaked travel copy

--- src/routes/travel/index.svelte
+++ src/routes/travel/index.svelte
@@ -7,13 +7,13 @@ import Accomodations from "../../components/Accomodations.svelte";
 <main>
     <div class='container'>
         <h1>Travel</h1>
-        <p>The two ways best ways to get here would probably be to fly into Boise... OR make a roadtrip out of it!</p>
+        <p>Fly or drive?</p>
         <h2>Fly</h2>
-        <p>You can fly into Boise International Airport. From there Ketchum is about 2.5 hours. We looked recently and found rental cars for about $30/day.</p>
+        <p>You can fly into Boise International Airport. From there, Ketchum is a 2.5 hours (3 hours if you take the scenic way, but it is SOOO worth it). We looked recently and found rental cars for about $30/day.</p>
         <h2>Roadtrip</h2>
         <p>If you are driving from WI you will see some incredible sights like:</p>
         <ul>
-            <li>Badlands National Park</li>
+            <li>Badlands National Park (about halfway</li>
             <li>Mount Rushmore</li>
             <li>Yellowstone National Park</li>
             <li>Grand Tetons National Park</li>
