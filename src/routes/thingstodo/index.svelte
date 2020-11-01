b54125afd4eb111b2d0fe0883a32af7faa997cd0
Andrew Petersen <andrew.petersen15@gmail.com>
2020-10-31 21:01:22 -0500

    a little copy

--- src/routes/thingstodo/index.svelte
+++ src/routes/thingstodo/index.svelte
@@ -2,5 +2,6 @@
     <div class='container'>
         <h1>Things to do</h1>
         <p>Coming soon!</p>
+        <p>We'll update this with cool things to do in the Boise & Ketchum area.</p>
     </div>
 </main>
